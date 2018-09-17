755d76dc4641dcfe0528a1800611392bf28e42e8
Allison Bolen <allisonbolen@Allisons-MBP.attlocal.net>
2018-09-16 20:50:07 -0400

    wait random amount of time

--- pcom.c
+++ pcom.c
@@ -41,11 +41,11 @@ int main(int argc, char*argv[]){
 }
 //install siguser1
 void sigusr1Handler (int sigNum){
-	signal(sigNum, SIG_IGN) // if we area already handeling the signal then we dont want to handle it if it triggers again while were handling it
+	signal(sigNum, SIG_IGN); // if we area already handeling the signal then we dont want to handle it if it triggers again while were handling it
 	puts("Signal 1 uesd?\n");
 }
 //install sigusr2
 void sigusr2Handler (int sigNum){
-	signal(sigNum, SIG_IGN)
+	signal(sigNum, SIG_IGN);
 	puts("Signal 1 uesd?\n");
 }
